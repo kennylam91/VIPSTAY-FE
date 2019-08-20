8deb46e6a8548b14e8a0e7581e0b34c8b1a9c698
kennylam91 <phamvanlam1991@gmail.com>
2019-08-20 15:26:13 +0700

    define HomePage Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {HomepageComponent} from './component/homepage/homepage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
