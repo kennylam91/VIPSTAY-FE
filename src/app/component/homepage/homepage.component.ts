cc3b748121b5d2437ca2c2dd89f8df791ad6d0ad
kennylam91 <phamvanlam1991@gmail.com>
2019-08-20 17:10:14 +0700

    get houses for homepage component

--- src/app/component/homepage/homepage.component.ts
+++ src/app/component/homepage/homepage.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HouseService} from '../../house.service';
+import {IHouse} from '../../model/IHouse';
 
 @Component({
   selector: 'app-homepage',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+  houses: IHouse[];
 
-  constructor() { }
+  constructor(private houseService: HouseService) {
+
+  }
 
   ngOnInit() {
+    this.houseService.getHouses()
+      .subscribe(next => {
+        this.houses = next;
+      });
   }
 
 }
