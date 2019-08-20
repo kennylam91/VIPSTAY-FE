d07f6748310a093a4805ce267a570affde3a238d
kennylam91 <phamvanlam1991@gmail.com>
2019-08-20 15:16:17 +0700

    create basic component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './component/homepage/homepage.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HouseDetailComponent } from './component/house-detail/house-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    FooterComponent,
+    HouseDetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/house-detail/house-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-house-detail',
+  templateUrl: './house-detail.component.html',
+  styleUrls: ['./house-detail.component.css']
+})
+export class HouseDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
