4f8e727d281e1a52ebab28ae2ef8c2a5fe87c596
kennylam91 <phamvanlam1991@gmail.com>
2019-08-22 11:22:01 +0700

    add IUser

---
+++ src/app/model/IUser.ts
@@ -0,0 +1,12 @@
+export interface IUser {
+  username: string;
+  password: string;
+  role: string;
+  name: string;
+  age: number;
+  address: string;
+  phone: string;
+  email: string;
+  id: number;
+  avatar: string;
+}
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -7,4 +7,5 @@ import {HttpClient} from '@angular/common/http';
 export class UserService {
 
   constructor(private httpClient: HttpClient) { }
+
 }
