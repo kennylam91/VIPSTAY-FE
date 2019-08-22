cc2732bdb6c699c5e4d1fe6b0a35d8553eac559a
kennylam91 <phamvanlam1991@gmail.com>
2019-08-22 16:52:51 +0700

    add file-upload component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomepageComponent } from './component/homepage/homepage.component';
-import { HeaderComponent } from './component/header/header.component';
-import { FooterComponent } from './component/footer/footer.component';
-import { HouseDetailComponent } from './component/house-detail/house-detail.component';
-import { SearchComponent } from './component/search/search.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomepageComponent} from './component/homepage/homepage.component';
+import {HeaderComponent} from './component/header/header.component';
+import {FooterComponent} from './component/footer/footer.component';
+import {HouseDetailComponent} from './component/house-detail/house-detail.component';
+import {SearchComponent} from './component/search/search.component';
 import {HttpClientModule} from '@angular/common/http';
-import { LoginComponent } from './component/login/login.component';
+import {LoginComponent} from './component/login/login.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+// @ts-ignore
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,4 +35,5 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
