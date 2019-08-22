452b4be60de8ceffd4952bf15a82ea6cca7602c6
kennylam91 <phamvanlam1991@gmail.com>
2019-08-22 09:58:16 +0700

    add basic validate

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -14,8 +14,8 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
     this.loginForm = this.formBuilder.group({
-        username: [''],
-        password: ['']
+        username: ['', Validators.required],
+        password: ['', Validators.required]
       }
     );
   }
