54a1f32454735c9ec1a48f9f60131a1598c1c941
kennylam91 <phamvanlam1991@gmail.com>
2019-08-22 08:44:43 +0700

    add logincomponent to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomepageComponent} from './component/homepage/homepage.component';
+import {LoginComponent} from './component/login/login.component';
 
 
 const routes: Routes = [
+  {path: 'login', component: LoginComponent},
   {path: '', component: HomepageComponent}
 ];
 
