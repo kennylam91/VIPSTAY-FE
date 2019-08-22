d76e5206c83b7c790be07f1ed737b047ce0e2116
kennylam91 <phamvanlam1991@gmail.com>
2019-08-22 11:35:31 +0700

    add method: post and put to user-service

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,11 +1,24 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {IUser} from './model/IUser';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private readonly API_URL = 'http://localhost:8080/api/houses';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {
+  }
+
+  create(user: Partial<IUser>): Observable<IUser> {
+    return this.httpClient.post<IUser>(this.API_URL, user);
+  }
+
+  update(user: IUser): Observable<IUser> {
+    return this.httpClient.put<IUser>(`${this.API_URL}/${user.id}`, user);
+  }
 
 }
+
