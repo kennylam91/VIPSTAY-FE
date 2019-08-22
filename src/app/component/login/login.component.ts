dfaab1d6ce4d7077fd8145ea6dc49d1651c3dbfa
kennylam91 <phamvanlam1991@gmail.com>
2019-08-22 09:25:23 +0700

    add login template

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private formBuilder: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+        username: [''],
+        password: ['']
+      }
+    );
+  }
+
+  login() {
+    console.log('Log in');
+    console.log(this.loginForm.value);
+  }
 }
