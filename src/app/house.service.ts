b93707a99df08d759633456fb927685bb4a956cf
kennylam91 <phamvanlam1991@gmail.com>
2019-08-23 17:38:52 +0700

    build login function

--- src/app/house.service.ts
+++ src/app/house.service.ts
@@ -1,18 +1,39 @@
 import {Injectable} from '@angular/core';
 import {IHouse} from './model/IHouse';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {AuthenticationService} from './authentication.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HouseService {
   houses: IHouse[];
+  header: Headers;
+  // httpOptions = {
+  //   headers: new HttpHeaders({
+  //     'Content-Type': 'application/json',
+  //     'Authorization', `Bearer ${this.authenService.token}`
+  //   })
+  // };
   private readonly API_URL = 'http://localhost:8080/api/houses';
 
   getHouses(): Observable<IHouse[]> {
-    return this.http.get<IHouse[]>(this.API_URL);
+    // this.header = new Headers({
+    //   Authorization: `Bearer ${this.authenService.token}`,
+    //   'Content-Type': 'application/json'
+    // });
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.authenService.token}`,
+      })
+    };
+    console.log(httpOptions);
+    return this.http.get<IHouse[]>(this.API_URL, httpOptions);
   }
+
   getHouseById(id: number): Observable<IHouse> {
     return this.http.get<IHouse>(`${this.API_URL}/${id}`);
   }
@@ -29,6 +50,9 @@ export class HouseService {
     return this.http.delete<IHouse>(`${this.API_URL}/${id}`);
   }
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private authenService: AuthenticationService) {
+
+    // this.httpOptions.headers.set('Authorization', `Bearer ${this.authenService.token}`);
+    console.log(this.header);
   }
 }
