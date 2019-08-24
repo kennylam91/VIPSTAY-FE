acd05ca4e31edd43a35eabab2a7edede2e34a320
kennylam91 <phamvanlam1991@gmail.com>
2019-08-24 10:08:33 +0700

    login and get houses: OK

--- src/app/house.service.ts
+++ src/app/house.service.ts
@@ -10,28 +10,15 @@ import {AuthenticationService} from './authentication.service';
 })
 export class HouseService {
   houses: IHouse[];
-  header: Headers;
-  // httpOptions = {
-  //   headers: new HttpHeaders({
-  //     'Content-Type': 'application/json',
-  //     'Authorization', `Bearer ${this.authenService.token}`
-  //   })
-  // };
   private readonly API_URL = 'http://localhost:8080/api/houses';
 
   getHouses(): Observable<IHouse[]> {
-    // this.header = new Headers({
-    //   Authorization: `Bearer ${this.authenService.token}`,
-    //   'Content-Type': 'application/json'
-    // });
-    const httpOptions = {
+    return this.http.get<IHouse[]>(this.API_URL, {
       headers: new HttpHeaders({
-        'Content-Type': 'application/json',
         Authorization: `Bearer ${this.authenService.token}`,
+        'Content-Type': 'application/json'
       })
-    };
-    console.log(httpOptions);
-    return this.http.get<IHouse[]>(this.API_URL, httpOptions);
+    });
   }
 
   getHouseById(id: number): Observable<IHouse> {
@@ -51,8 +38,5 @@ export class HouseService {
   }
 
   constructor(private http: HttpClient, private authenService: AuthenticationService) {
-
-    // this.httpOptions.headers.set('Authorization', `Bearer ${this.authenService.token}`);
-    console.log(this.header);
   }
 }
