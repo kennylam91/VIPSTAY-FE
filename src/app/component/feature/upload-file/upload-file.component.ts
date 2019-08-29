6acb9b08de807c1f35352ed45c0040526ceb1515
elnino228 <daodat228@gmail.com>
2019-08-29 14:56:34 +0700

    update

--- src/app/component/feature/upload-file/upload-file.component.ts
+++ src/app/component/feature/upload-file/upload-file.component.ts
@@ -29,42 +29,35 @@ export class UploadFileComponent implements OnInit {
     console.log(event.target.files);
     const files = event.target.files;
     for (const file of files) {
-      const id = Math.random().toString(36).substring(2); // Create a random string
-      this.ref = this.afStorage.ref(id);
-      this.ref.put(file)
-        .then(snapshot => {
-          return snapshot.ref.getDownloadURL();   // Will return a promise with the download link
-        })
-        .then(downloadURL => {
-          this.houseService.imageUrls.push(downloadURL);
-          // this.giveURLtoCreate.emit(this.downloadURL);
-          // console.log(downloadURL);
-          // return downloadURL;
-        })
-        .catch(error => {
-          // Use to signal error if something goes wrong.
-          console.log(`Failed to upload file and get link - ${error}`);
-        });
+      this.onUpload(file);
     }
     console.log(this.houseService.imageUrls);
   }
 
-  onUpload() {
-    const id = Math.random().toString(36).substring(2); // Create a random string
-    this.ref = this.afStorage.ref(id);
-    this.ref.put(this.selectedFile)
-      .then(snapshot => {
-        return snapshot.ref.getDownloadURL();   // Will return a promise with the download link
-      })
-      .then(downloadURL => {
-        this.downloadURL = downloadURL;
-        this.giveURLtoCreate.emit(this.downloadURL);
-        // console.log(downloadURL);
-        return downloadURL;
-      })
-      .catch(error => {
-        // Use to signal error if something goes wrong.
-        console.log(`Failed to upload file and get link - ${error}`);
-      });
+  async onUpload(file: File) {
+    try {
+      const id = Math.random().toString(36).substring(2); // Create a random string
+      this.ref = this.afStorage.ref(id);
+      const snapshot = await this.ref.put(file);
+      const downloadUrl = await snapshot.ref.getDownloadURL();
+      return downloadUrl;
+    } catch (error) {
+      console.log(`Failed to upload file and get link - ${error}`);
+    }
+
+    // this.ref.put(file)
+    //   .then(snapshot => {
+    //     return snapshot.ref.getDownloadURL();   // Will return a promise with the download link
+    //   })
+    //   .then(downloadURL => {
+    //     this.downloadURL = downloadURL;
+    //     this.giveURLtoCreate.emit(this.downloadURL);
+    //     // console.log(downloadURL);
+    //     return downloadURL;
+    //   })
+    //   .catch(error => {
+    //     // Use to signal error if something goes wrong.
+    //     console.log(`Failed to upload file and get link - ${error}`);
+    //   });
   }
 }
